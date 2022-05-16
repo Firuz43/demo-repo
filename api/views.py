aad40658e4039d3bd5021c52e4ca761b84fe30a5
Firuz43 <firuz1199@mail.ru>
2022-05-16 11:04:55 +0400

    DRF

---
+++ api/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Item
+
+# Create your views here.
+
+
+@api_view(['GET'])
+def apiView(request):
+    api_urls = {
+        'all_items': '/',
+        'Search by Category': '/?category=category_name',
+        'Search by Subcategory': '/?subcategory=category_name',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/item/pk/delete'
+    }
+
+    return Response(api_urls)
--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'api',
     'app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
