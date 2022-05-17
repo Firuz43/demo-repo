484d16f7f83f433c2ca5d229f134fac5818ebe8a
Firuz43 <firuz1199@mail.ru>
2022-05-17 23:00:16 +0400

    txt

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiView, name="home")
+    path('', views.apiView, name="home"),
+    path('create/', views.addItem, name="addItem"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+
+from api.serializers import ItemSerializer
 from .models import Item
 
 # Create your views here.
@@ -18,3 +22,17 @@ def apiView(request):
     }
 
     return Response(api_urls)
+
+
+@api_view(['POST'])
+def addItem(request):
+    item = ItemSerializer(data=request.data)
+
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
